0e4860fc8fa974fd77a0c7350b77b9c66c15cf6d
sulubin <sulubin123@163.com>
2016-08-26 15:18:39 +0800

    上线多线程日志收集工具(部分更新)

--- src/main/scala/com/changtu/service/CollectMonitor.scala
+++ src/main/scala/com/changtu/service/CollectMonitor.scala
@@ -6,11 +6,12 @@ import com.changtu.util.Logging
 
 /**
   * Created by lubinsu on 8/23/2016.
+  * 命令派发
   */
 class CollectMonitor extends Actor with Logging{
   override def receive: Receive = {
     case Host(host, port, cmd) =>
-      val collector = context.actorOf(Props[CollectLogService], "collector".concat(host))
+      val collector = context.actorOf(Props[CollectLogService], "collector-".concat(host))
       collector ! Host(host, port, cmd)
     case _ => logger.warn("unknown operation.")
   }
